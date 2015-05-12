d7a41a390dd18343c4293e483d3cc93c9fdf7796
Rosemary Trahan <rat@micro-mouse.com>
2015-05-11 20:28:42 -0500

    initial commit

---
+++ js/resumebuilder.js
@@ -0,0 +1,269 @@
+var bio = {
+  "name": "Rosemary Trahan",
+  "role": "Learning & Development Specialist",
+  "contacts": {
+    "mobile": "512-555-5555",
+    "email": "rat@micro-mouse.com",
+    "twitter": "@ratrahan",
+    "github": "ratrahan",
+    "blog": "ratrahanblog.wordpress.com"
+  },
+  "welcomeMessage": "<br>Nam sint velit nulla vidisse, si sed culpa dolore illum a iis magna appellat ab sint voluptate o officia, irure et sed irure offendit a senserit amet aut appellat reprehenderit, a summis cillum qui expetendis in cillum quo pariatur qui ipsum. Do id magna varias lorem o ita quid do ipsum non nisi appellat quo dolore legam, eiusmod ipsum quibusdam. Ut singulis in litteris. Ne elit irure culpa nescius ea an varias eu quis sed quem do an irure offendit, pariatur sunt doctrina.",
+  "skills": ["Instructional Designer", "eLearning Developer", "Web Front-End Developer", "Project Manager"],
+  "bioPic": "images/voki.png"
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "ACME Brick",
+      "title": "Project Manager - Enterprise Software Implementation",
+      "location": "Houston, TX",
+      "dates": "September 2010 – May 2013",
+      "description": "Do id magna varias lorem o ita quid do ipsum non nisi appellat quo dolore legam, eiusmod ipsum quibusdam. Culpa hic senserit do lorem a amet appellat consectetur nam voluptate a sint, anim occaecat litteris ea in sint probant an incurreret summis aut mentitum instituendarum o elit arbitrantur nescius quorum admodum, ne aliquip praetermissum."
+        }, {
+      "employer": "ACME Builders",
+      "title": "Senior Systems Administrator",
+      "location": "Dallas, TX",
+      "dates": "June 2008 – August 2010",
+      "description": "A nisi vidisse quibusdam. Nam eram senserit domesticarum e singulis ab lorem consequat iis illum singulis laborum ubi e a fugiat culpa fugiat, aliqua e te tamen occaecat et excepteur tractavissent aut aliquip, quis laborum nostrud e noster possumus te transferrem. Sed velit consequat. O fugiat expetendis tempor, litteris ut multos. Labore offendit relinqueret, sunt est ullamco se varias est si quem varias fugiat cupidatat eu iis esse commodo admodum."
+        }, {
+      "employer": "John Doe & Sons",
+      "title": "Systems Analyst",
+      "location": "San Antonio, TX",
+      "dates": "August 2007 – May 2008",
+      "description": "Esse et voluptate sed tamen, ea aliqua fidelissimae, dolor senserit quo domesticarum sed admodum exercitation ea eiusmod. Officia anim sunt fabulas irure id nulla nescius sed mentitum, iudicem irure dolore id labore, e nulla praesentibus. Aute ubi commodo. Aute ita ad anim vidisse. Labore arbitror  transferrem nam ea summis ab velit, ab veniam admodum, singulis a offendit. Ea quem voluptate, nulla ex fabulas est amet iis laboris illum irure non aute te sed tamen de esse. Et qui quis sunt duis."
+        }, {
+      "employer": "Jane Doe & Daughters",
+      "title": "Software Developer",
+      "location": "Austin, TX",
+      "dates": "September 2001 – July 2007",
+      "description": "Excepteur si dolore cupidatat. Cillum nam quibusdam te legam ab iudicem minim appellat mandaremus, ita a aliqua officia, summis exquisitaque mandaremus aute voluptate. Se si magna quorum malis hic excepteur malis cillum possumus veniam aut enim ut vidisse sed multos, ea anim nescius probant, nisi offendit consectetur ad ullamco te quae ex irure officia an comprehenderit, singulis aute dolore cupidatat aute. Officia iis multos ne senserit familiaritatem quo commodo id te fugiat arbitrantur, aliqua e mandaremus ea irure."
+        }
+    ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "IT Project Manager",
+      "dates": "September 2010 – May 2013",
+      "description": "Cillum sed proident, malis relinqueret eiusmod esse nostrud. Ex culpa consequat o incurreret do aliqua de amet arbitrantur fabulas quem nostrud hic pariatur eu legam nescius ubi iis irure e dolor ne nisi iis aut minim consequat nam incididunt ex eram et laborum de cillum. Voluptate ad ullamco sed senserit praesentibus ex litteris, o quis philosophari id et cillum aut magna, hic multos esse quae mentitum iis aute singulis ex laboris, summis pariatur comprehenderit, aut aliqua offendit efflorescere. Eu amet eiusmod efflorescere iis enim efflorescere cernantur quem vidisse ubi ubi ipsum incididunt litteris, ingeniis amet officia laboris, cernantur non culpa in de in enim sint illum, de enim lorem nulla proident si appellat ne occaecat. An cillum occaecat. Mandaremus lorem fugiat possumus illum. Nescius fore o litteris firmissimum an amet vidisse a possumus.",
+      "images": [
+        "http://placehold.it/400x200",
+        "http://placehold.it/400x200"
+      ]
+    }, {
+      "title": "IT Project Manager",
+      "dates": "June 2008 – August 2010",
+      "description": "Incididunt ea ullamco, non duis quae summis voluptate. Possumus fugiat veniam excepteur fugiat se ea labore se elit. Elit voluptate ad sunt quis id ad varias occaecat praetermissum. Te sunt ex sint nam minim quo qui magna mentitum. De multos proident instituendarum, fugiat an iudicem eu velit domesticarum laboris eram singulis o ab iis legam iudicem qui nam varias enim te admodum de iudicem reprehenderit si mandaremus, quem litteris qui nostrud ne ex ea consectetur.",
+      "images": [
+        "http://placehold.it/400x200",
+        "http://placehold.it/400x200"
+      ]
+    }, {
+      "title": "IT Project Manager",
+      "dates": "August 2007 – May 2008",
+      "description": "Singulis quid cupidatat commodo, singulis quid enim ad minim do excepteur transferrem e cupidatat, noster arbitror sed fugiat sunt, et dolor expetendis incididunt, expetendis quorum officia o se minim fabulas, ab fugiat proident. Senserit fore singulis, cupidatat hic litteris id nescius ne quid qui est ubi nisi singulis. E cillum officia, incurreret amet vidisse. Id nam esse duis aliqua et ut te legam senserit ex summis nostrud a reprehenderit, mentitum malis occaecat, in ubi enim consequat eu consequat aut amet mandaremus, arbitror summis appellat, est in anim duis summis. Aut minim comprehenderit, a duis singulis ingeniis, aliquip sunt senserit, hic mandaremus quo admodum an cernantur noster quem et enim eu mandaremus culpa excepteur tempor ad ut aut fidelissimae, veniam incurreret firmissimum. Illum incurreret et imitarentur si ne mentitum ex incurreret, illum eu voluptate, quamquam legam cillum quo fore.",
+      "images": [
+        "http://placehold.it/400x200",
+        "http://placehold.it/400x200"
+      ]
+    }
+  ]
+}
+
+
+var education = {
+  "schools": [
+    {
+      "name": "University of North Texas",
+      "city": "Denton, TX, US",
+      "degree": "Masters",
+      "majors": ["Learning Technologies", "Instructional Design"],
+      "dates": "2013 - 2014",
+      "URL": "unt.edu"
+    }, {
+      "name": "University of Houston",
+      "city": "Houston, TX, US",
+      "degree": "Bachelors",
+      "majors": ["Psychology", "Sociology"],
+      "dates": "1995 - 2000",
+      "URL": "uh.edu"
+    }
+   ],
+  "online_courses": [
+    {
+      "title": "Front-End Developer Nanodegree",
+      "school": "Udacity",
+      "dates": "2015",
+      "URL": "udacity.com"
+    },
+    {
+      "title": "Front-End Developer Series",
+      "school": "Treehouse",
+      "dates": "2015",
+      "URL": "teamTreehouse.com"
+    }
+  ]
+}
+
+// work
+
+work.display = function() {
+
+  $("#workExperience").append(HTMLworkStart);
+
+  for (job in work.jobs) {
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    $(".work-entry:last").append(formattedLocation);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
+}
+
+// project
+
+projects.display = function() {
+
+  for (project in projects.projects) {
+
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedprojectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedprojectTitle);
+
+    var formattedprojectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedprojectDates);
+
+    var formattedprojectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedprojectDescription);
+
+    if (projects.projects[project].images.length > 0) {
+
+        for (image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+        }
+    }
+  }
+}
+
+// education
+
+education.display = function() {
+
+  $("#education").append(HTMLschoolStart);
+
+  for (school in education.schools) {
+    var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+    $(".education-entry:last").append(formattedschoolName);
+
+    var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+    $(".education-entry:last").append(formattedLocation);
+
+    var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+    $(".education-entry:last").append('&#160;&#160;--&#160;' + formattedschoolDegree);
+
+    var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+    $(".education-entry:last").append(formattedschoolDates);
+
+    for (major in education.schools[school].majors) {
+
+      var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]);
+      $(".education-entry:last").append(formattedschoolMajor);
+    }
+
+    var formattedURL = HTMLschoolURL.replace("%data%", education.schools[school].URL)
+    $(".education-entry:last").append(formattedURL);
+  }
+
+    if (education.online_courses.length > 0) {
+
+      $(".education-entry:last").append(HTMLonlineClasses);
+
+    }
+
+    for (online_course in education.online_courses) {
+
+      var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.online_courses[online_course].school);
+      $(".education-entry:last").append(formattedonlineSchool);
+
+      var formattedonlineDates = HTMLonlineDates.replace("%data%", education.online_courses[online_course].dates);
+      $(".education-entry:last").append(formattedonlineDates);
+
+      var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.online_courses[online_course].title);
+      $(".education-entry:last").append(formattedonlineTitle);
+
+      var formattedonlineURL = HTMLonlineURL.replace("%data%", education.online_courses[online_course].URL);
+      $(".education-entry:last").append(formattedonlineURL);
+
+    }
+}
+
+// build webpage using jQuery
+
+// Name & Role
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedName);
+
+// Contact Information
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+
+var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+$("#topContacts").append(formattedBlog);
+
+
+// picture & welcome message
+
+var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedbioPic);
+
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#header").append(formattedWelcomeMsg);
+
+// skills
+
+if (bio.skills.length > 0) {
+  $("#header").append(HTMLskillsStart);
+}
+
+for (skill in bio.skills) {
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+  $("#skills").append(formattedSkill);
+}
+
+// Work history
+work.display();
+
+// Project history
+projects.display();
+
+// Educational History
+education.display();
+
